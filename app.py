e791aeb748acb3180a4a6b18628831032110f31b
liu183 <52588199+liu183@users.noreply.github.com>
2025-03-03 22:56:07 +0800

    j加入groq的快速服务

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from groq import Groq
 
 # 加载环境变量
 load_dotenv()
@@ -10,10 +11,6 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-client = OpenAI(
-    base_url="https://integrate.api.nvidia.com/v1",
-    api_key=os.getenv("NVIDIA_API_KEY")
-)
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
@@ -21,13 +18,20 @@ def chat():
         data = request.json
         user_message = data.get('message')
         
+        client = Groq()
         completion = client.chat.completions.create(
-            model="deepseek-ai/deepseek-r1",
-            messages=[{"role": "user", "content": user_message}],
+            model="deepseek-r1-distill-qwen-32b",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ],
             temperature=0.6,
-            top_p=0.7,
-            max_tokens=4096,
-            stream=True
+            max_completion_tokens=4096,
+            top_p=0.95,
+            stream=True,
+            stop=None,
         )
         
         def generate():
